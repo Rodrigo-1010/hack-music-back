d1b464eed148d835c1378f1bfd6e50f27c242be1
goldvinda <104211952+goldvinda@users.noreply.github.com>
2022-06-22 15:51:08 -0300

    add category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,21 +2,62 @@ const Category = require("../models/Category");
 
 // Display a listing of categories.
 async function index(req, res) {
-  const categories = await Category.find();
-  res.json({ msg: "Hola" });
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
 }
 
 // Display the specified category.
-async function show(req, res) {}
+async function show(req, res) {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+}
 
 // Store a newly created category in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  try {
+    const newCategory = await Category.create({
+      name: req.body.name,
+      description: req.body.description,
+      picture: req.body.picture,
+      slug: req.body.slug,
+    });
+    res.status(201).json(newCategory);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+}
 
 // Update the specified category in storage.
-async function update(req, res) {}
+async function update(req, res) {
+  const { name, description, picture, slug } = req.body;
+  try {
+    const updatedCategory = await Category.findByIdAndUpdate(
+      { _id: req.params.id },
+      { name, description, picture, slug },
+    );
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+}
 
 // Remove the specified category from storage.
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  try {
+    const deletedCategory = await Category.findByIdAndDelete(req.params.id);
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+}
 
 module.exports = {
   index,
