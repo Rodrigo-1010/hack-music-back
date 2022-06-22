b0ada2b91347dc51ac8da0114980f808d8fe37f7
goldvinda <104211952+goldvinda@users.noreply.github.com>
2022-06-22 14:44:02 -0300

    fix  category schema

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
   buyer: String, // ID que referencia al usuario que realizo la compra
-  products: { type: [], ref: Product }, //array de objetos con: id, product, quantity, checkoutPrice
+  products: [{ type: Schema.Types.ObjectId, ref: "product" }], //array de objetos con: id, product, quantity, checkoutPrice
   status: Stringn,
   totalPrice: Number,
 });
