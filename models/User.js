f7b8798310f2abe7fc7b635ddf98f32637f97c5b
Rodrigo <rodrigom301294@gmail.com>
2022-06-23 09:24:41 -0300

    Updating hash logic to async/  await

--- models/User.js
+++ models/User.js
@@ -12,25 +12,14 @@ const userSchema = new Schema({
   orders: [{ type: Schema.ObjectId, ref: "Order" }], //array con id de referencias a Order
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   const user = this;
 
-  // only hash the password if it has been modified (or is new)
+  //Only hash the password if it has been modified (or is new)
   if (!user.isModified("password")) return next();
-
-  // generate a salt
-  bcrypt.genSalt(10, function (err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function (err, hash) {
-      if (err) return next(err);
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
+  const hash = await bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  next();
 });
 
 // userSchema.methods.comparePassword = function (candidatePassword, cb) {
