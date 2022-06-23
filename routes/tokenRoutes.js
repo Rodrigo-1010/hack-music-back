3a96e383f7b4fd2fd318aba35b161b62b4280662
Rodrigo <rodrigom301294@gmail.com>
2022-06-23 10:31:17 -0300

    Token route

--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const tokensRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-tokensRouter.post("/tokens", async function sendToken(req, res) {});
+tokensRouter.post("/tokens", async function sendToken(req, res) {
+  try {
+    const user = await User.findById(?);
+  } catch (err) {}
+
+  const token = jwt.sign(user, process.env.JWT_SECRET_KEY);
+});
 
 module.exports = tokensRouter;
