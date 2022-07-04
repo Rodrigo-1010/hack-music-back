d0872c52917485871d21d586aeb699a6632ef812
Rodrigo <rodrigom301294@gmail.com>
2022-07-04 12:41:54 -0300

    MongoDB Atlas config

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const seeder = require("./seeders/seeder");
 const categorySeeder = require("./seeders/categorySeeder");
 
 module.exports = async function dbSetup() {
-  mongoose.connect("mongodb://localhost:27017/hack-music");
+  mongoose.connect(process.env.MONGODB_ATLAS_CONNECTION);
   mongoose.connection
     .once("open", () => console.log("¡Conexión con la base de datos establecida!"))
     .on("error", (error) => console.log(error));
