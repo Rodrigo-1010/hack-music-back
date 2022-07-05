7cfc9478516497c06fd584c5bf4341e5ca1f7601
Gabriel Bartesaghi <g.bartesaghi25@gmail.com>
2022-07-05 18:22:37 -0300

    Add category header img

--- seeders/categories.js
+++ seeders/categories.js
@@ -1,29 +1,34 @@
 const categoriesSeed = [
-    {
-      name: "drums-and-percussion",
-      alias: "Drums & Percussion",
-      img: "https://i.postimg.cc/d1H3H9wF/category-card-percussion.jpg",
-    },
-    {
-      name: "guitar-and-bass",
-      alias: "Guitar & Bass",
-      img: "https://i.postimg.cc/gk6rrjfV/category-card-strings.jpg",
-    },
-    {
-      name: "wind-instruments",
-      alias: "Wind instruments",
-      img: "https://i.postimg.cc/vZFTnMsp/category-card-wind.jpg",
-    },
-    {
-      name: "keyboards-and-pianos",
-      alias: "Keyboards & Pianos",
-      img: "https://i.postimg.cc/NGn5gmPG/category-card-keyboards.jpg",
-    },
-    {
-      name: "accessories",
-      alias: "Accessories",
-      img: "https://i.postimg.cc/ZnXRVzyv/category-card-accessories.jpg",
-    },
-  ];
+  {
+    name: "drums-and-percussion",
+    alias: "Drums & Percussion",
+    img: "https://i.postimg.cc/d1H3H9wF/category-card-percussion.jpg",
+    headerImg: "https://i.postimg.cc/V61svYzZ/pexels-caleb-oquendo-3018083.jpg",
+  },
+  {
+    name: "guitar-and-bass",
+    alias: "Guitar & Bass",
+    img: "https://i.postimg.cc/gk6rrjfV/category-card-strings.jpg",
+    headerImg: "https://i.postimg.cc/pVC0wxp7/guitar-and-bass.jpg",
+  },
+  {
+    name: "wind-instruments",
+    alias: "Wind instruments",
+    img: "https://i.postimg.cc/vZFTnMsp/category-card-wind.jpg",
+    headerImg: "https://i.postimg.cc/BZ5xyCwn/wind.jpg",
+  },
+  {
+    name: "keyboards-and-pianos",
+    alias: "Keyboards & Pianos",
+    img: "https://i.postimg.cc/NGn5gmPG/category-card-keyboards.jpg",
+    headerImg: "https://i.postimg.cc/2y3Trbt5/keyboard.jpg",
+  },
+  {
+    name: "accessories",
+    alias: "Accessories",
+    img: "https://i.postimg.cc/ZnXRVzyv/category-card-accessories.jpg",
+    headerImg: "https://i.postimg.cc/4xYPr992/cat-marquee-aira-compact.jpg",
+  },
+];
 
-  module.exports = categoriesSeed
+module.exports = categoriesSeed;
