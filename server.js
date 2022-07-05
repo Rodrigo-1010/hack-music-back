dc4d772f833950fb11d7563b0bceefbc86fbcc85
Rodrigo <rodrigom301294@gmail.com>
2022-07-05 19:20:28 -0300

    Formidable config and install

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 const routes = require("./routes");
 const cors = require("cors");
 const dbSetup = require("./db");
+const { createClient } = require("@supabase/supabase-js");
 dbSetup();
+
+// .SUPABASE
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 app.use(cors());
 app.use(express.json());
