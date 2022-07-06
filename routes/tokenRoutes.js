64f5805009592879ff796119a292aba022695369
Rodrigo <rodrigom301294@gmail.com>
2022-07-06 17:10:05 -0300

    Route fix

--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -5,6 +5,21 @@ const jwt = require("jsonwebtoken");
 const Admin = require("../models/Admin");
 const User = require("../models/User");
 
+tokensRouter.post("/tokens/admin", async function sendToken(req, res) {
+  try {
+    const admin = await Admin.findOne({ email: req.body.email });
+    if (!admin) return res.status(400).json({ msg: "Credenciales no validas." });
+    const match = await admin.comparePassword(req.body.password);
+    if (!match) return res.status(400).json({ msg: "Credenciales no validas." });
+    return res.json({
+      firstName: admin.firstName,
+      token: jwt.sign({ email: admin.email }, process.env.JWT_SECRET_KEY),
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 tokensRouter.post("/tokens", async function sendToken(req, res) {
   if (req.body.email.includes("@admin")) {
     try {
@@ -33,21 +48,6 @@ tokensRouter.post("/tokens", async function sendToken(req, res) {
     } catch (err) {
       console.log(err);
     }
-  }
-});
-
-tokensRouter.post("/tokens/admin", async function sendToken(req, res) {
-  try {
-    const admin = await Admin.findOne({ email: req.body.email });
-    if (!admin) return res.status(400).json({ msg: "Credenciales no validas." });
-    const match = await admin.comparePassword(req.body.password);
-    if (!match) return res.status(400).json({ msg: "Credenciales no validas." });
-    return res.json({
-      firstName: admin.firstName,
-      token: jwt.sign({ email: admin.email }, process.env.JWT_SECRET_KEY),
-    });
-  } catch (err) {
-    console.log(err);
   }
 });
 
