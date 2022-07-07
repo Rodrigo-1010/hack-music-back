2f14ae42e3c0b38bf8df02b4764f774519797155
Rodrigo <rodrigom301294@gmail.com>
2022-07-07 09:31:01 -0300

    reset db route fix

--- routes/resetRoutes.js
+++ routes/resetRoutes.js
@@ -3,7 +3,7 @@ const seeder = require("../seeders/seeder");
 const resetRouter = express.Router();
 
 resetRouter.get("/reset", async (req, res) => {
-  seeder();
+  await seeder();
   res.status(200).json({ msg: "Database succesfully reset." });
 });
 
