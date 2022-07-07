3cec96fe662fd322df66ce30ac66dce53155521f
Rodrigo <rodrigom301294@gmail.com>
2022-07-07 19:21:59 -0300

    User routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,10 @@ const { expressjwt: jwt } = require("express-jwt");
 
 userRouter.get("/users", userController.index);
 userRouter.get("/users/:id", userController.show);
+userRouter.post("/users", userController.store);
 
 userRouter.use("/users", jwt({ secret: process.env.JWT_SECRET_KEY, algorithms: ["HS256"] }));
 
-userRouter.post("/users", userController.store);
 userRouter.patch("/users/:id", userController.update);
 userRouter.delete("/users/:id", userController.destroy);
 
